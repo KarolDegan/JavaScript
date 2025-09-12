ffdaebfbf35cd3671db55d6ab535a64bf11f5fb4
Karol <karol.degan@hotmail.com>
2025-09-12 14:45:50 -0300

    final aula 2 manipulando elementos dom

--- alura/manipulando_elementos_DOM/script.js
+++ alura/manipulando_elementos_DOM/script.js
@@ -5,6 +5,7 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 
 const imagem = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 /*focoBt.addEventListener('click', () =>{
     html.setAttribute('data-contexto', 'foco')
@@ -26,4 +27,17 @@ longoBt.addEventListener('click', () => {
 function alterarContexto(contexto){
     html.setAttribute('data-contexto', contexto);
     imagem.setAttribute('src', `./imagens/${contexto}.png`);
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada? <br> <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície. <br> <strong class="app__title-strong"> Faça uma pausa longa. </strong>`
+        default:
+            break;
+    }
 }
