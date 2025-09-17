83be64ec834a3d352ddd46ff6590d9099d28769f
Karol <karol.degan@hotmail.com>
2025-09-17 14:32:27 -0300

    quart

--- alura/menipule_DOM_com_JavaScript/main.js
+++ alura/menipule_DOM_com_JavaScript/main.js
@@ -15,17 +15,28 @@
         conteudo = `<p class="content">${valorEntrada}</p>`
         tarefa.innerHTML = conteudo;
 
-        tarefa.appendChild(BotaoConclui())
+        tarefa.appendChild(BotaoConclui());
+        tarefa.appendChild(BotaoDeleta());
         entrada.value = " ";
 
     }
 
-    const eventoConcluido = (evento) => {
+    const tarefaConcluida = (evento) => {
         const botaoConclui = evento.target;
 
         const tarefaConcluida = botaoConclui.parentElement;
 
         tarefaConcluida.classList.toggle('done');
+    }
+
+    const deletarTarefa = (evento) =>{
+        const ondefoi = evento.target;
+
+        const elementoPai = ondefoi.parentElement;
+
+        elementoPai.remove();
+
+
     }
 
     const botao = document.querySelector('[data-form-buttom]');
@@ -39,8 +50,18 @@
 
         botaoConclui.classList.add('check-button');
         botaoConclui.innerText = 'Concluir';
-        botaoConclui.addEventListener('click', eventoConcluido);
+        botaoConclui.addEventListener('click', tarefaConcluida);
 
         return botaoConclui;
+    }
+
+    const BotaoDeleta = () => {
+        const botaoDeleta = document.createElement('button');
+
+        botaoDeleta.classList.add('check-button');
+        botaoDeleta.innerText = 'deletar';
+        botaoDeleta.addEventListener('click', deletarTarefa);
+
+        return botaoDeleta;
     }
 })()
