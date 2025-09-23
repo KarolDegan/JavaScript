9c197a4b095f1adc006eaf192c677ef968115854
Karoline Oliveira Degan <karol.degan@hotmail.com>
2025-09-22 23:47:29 -0300

    fim da aula 2 da parte 3

--- alura/typescript/parte3/app/dist/js/views/view.js
+++ alura/typescript/parte3/app/dist/js/views/view.js
@@ -4,6 +4,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-execucao.js";
 export class View {
     constructor(seletor, escapar) {
@@ -21,13 +22,10 @@ export class View {
     }
     update(model) {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 }
 __decorate([
-    logarTempoDeExecucao(true)
+    logarTempoDeExecucao(true),
+    inspect
 ], View.prototype, "update", null);
--- alura/typescript/parte3/app/src/views/view.ts
+++ alura/typescript/parte3/app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-execucao.js";
 
 export abstract class View<T> {
@@ -18,13 +19,14 @@ export abstract class View<T> {
     }
 
     @logarTempoDeExecucao(true)
+    @inspect
     public update(model: T): void {
         //const t1 = performance.now();
         let template = this.template(model);
-        if (this.escapar) {
+        /*if (this.escapar) {
             template = template
                 .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
+        }*/
         this.elemento.innerHTML = template;
         //const t2 = performance.now();
         //console.log(`Tempo de execução do método update: ${(t2-t1)/1000} segundos`)
